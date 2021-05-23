8b353188584dd538df2426b1d82a0ef74aa4c5fd
Roman Bachmann <Roman.Bachmann@swisscom.com>
2021-05-23 21:55:14 +0200

    Bump version

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+rm -rf build/libs/
 ./gradlew assemble
-docker buildx build --platform linux/arm64 -t robachmann/netatmo-exporter:1.2.0-arm64 --push .
+docker buildx build --platform linux/arm64 -t robachmann/netatmo-exporter:1.2.1-arm64 --push .
